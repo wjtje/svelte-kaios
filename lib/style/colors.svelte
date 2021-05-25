8b94cd194606c6c14294b1da211c0feff4d7c09f
Wouter van der Wal <33957974+wjtje@users.noreply.github.com>
2021-05-25 15:24:39 +0200

    💄 Changed color names and added the colors to a svelte file

---
+++ lib/style/colors.svelte
@@ -0,0 +1,39 @@
+<script lang="ts" context="module">
+  export type KaiOS_color = [string, string];
+
+  // Grayscale
+  export const GS00: KaiOS_color = ["#ffffff", "#000000"];
+  export const GS10: KaiOS_color = ["#e6e6e6", "#000000"];
+  export const GS20: KaiOS_color = ["#cccccc", "#000000"];
+  export const GS45: KaiOS_color = ["#8c8c8c", "#ffffff"];
+  export const GS70: KaiOS_color = ["#6a6a6a", "#ffffff"];
+  export const GS90: KaiOS_color = ["#323232", "#ffffff"];
+  export const GS100: KaiOS_color = ["#000000", "#ffffff"];
+
+  // Primary colors
+  export const PURPLE: KaiOS_color = ["#8000ff", "#ffffff"];
+  export const PINK: KaiOS_color = ["#e60060", "#ffffff"];
+  export const LIME: KaiOS_color = ["#00bf60", "#ffffff"];
+  export const CYAN: KaiOS_color = ["#1cc1c4", "#ffffff"];
+  export const BLUE: KaiOS_color = ["#0073e6", "#ffffff"];
+  export const ORANGE: KaiOS_color = ["#ff4f1a", "#ffffff"];
+  export const RED: KaiOS_color = ["#d90036", "#ffffff"];
+
+  // Mid-dark colors
+  export const MID_DARK_PURPLE: KaiOS_color = ["#5900b3", "#ffffff"];
+  export const MID_DARK_PINK: KaiOS_color = ["#990040", "#ffffff"];
+  export const MID_DARK_LIME: KaiOS_color = ["#007339", "#ffffff"];
+  export const MID_DARK_CYAN: KaiOS_color = ["#08888a", "#ffffff"];
+  export const MID_DARK_BLUE: KaiOS_color = ["#004d99", "#ffffff"];
+  export const MID_DARK_ORANGE: KaiOS_color = ["#a72600", "#ffffff"];
+  export const MID_DARK_RED: KaiOS_color = ["#b4002d", "#ffffff"];
+
+  // Dark colors
+  export const DARK_PURPLE: KaiOS_color = ["#2a0361", "#ffffff"];
+  export const DARK_PINK: KaiOS_color = ["#3d041c", "#ffffff"];
+  export const DARK_LIME: KaiOS_color = ["#00462c", "#ffffff"];
+  export const DARK_CYAN: KaiOS_color = ["#045463", "#ffffff"];
+  export const DARK_BLUE: KaiOS_color = ["#012b68", "#ffffff"];
+  export const DARK_ORANGE: KaiOS_color = ["#36150c", "#ffffff"];
+  export const DARK_RED: KaiOS_color = ["#530015", "#ffffff"];
+</script>
